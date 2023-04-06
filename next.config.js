1f94b517cba4a4e90fa56dbb67ffb49595bc0fc8
nikolai-sim <nikolai_sim@hotmail.com>
2023-04-07 11:39:43 +1200

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compress: true,
+  async headers() {
+    return [
+      {
+        source: "/:all*(gz)",
+        locale: false,
+        headers: [
+          {
+            key: "Content-Encoding",
+            value: "gzip",
+          },
+          {
+            key: "Content-Type",
+            value: "application/gzip",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
